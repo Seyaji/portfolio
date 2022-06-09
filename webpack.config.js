ee8bf9e0bbbcac6a972ca9f7ac135c824f06d0c7
Alex <alex@Alexs-MBP.lan>
2022-06-09 21:23:53 +0100

    completed basic setup

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ const stylesHandler = isProduction ? MiniCssExtractPlugin.loader : "style-loader
 const config = {
     entry: "./src/index.ts",
     output: {
-        path: path.resolve(__dirname, "dist"),
+        path: path.resolve(__dirname, "public"),
     },
     // devtool:'source-map',
     devServer: {
