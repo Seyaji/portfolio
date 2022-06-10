f619f6b3c401909909c579bcaa155799a730e268
Alex <alex@Alexs-MBP.lan>
2022-06-10 20:17:53 +0100

    added backgrounds to card and contact sections

--- src/shapes/orb.ts
+++ src/shapes/orb.ts
@@ -3,14 +3,12 @@ import { OrbLike } from "../types";
 import SimplexNoise from "simplex-noise";
 import debounce from "debounce";
 
-import { random, map } from "../uitls/math"
+import { random, map } from "../uitls/math";
 
-const simplex = new SimplexNoise
-
+const simplex = new SimplexNoise();
 
 // Orb class
 export class Orb extends OrbLike {
-
     constructor(fill = 0x000000) {
         super();
         this.bounds = this.setBounds();
@@ -38,8 +36,8 @@ export class Orb extends OrbLike {
     }
     setBounds() {
         const maxDist = window.innerWidth < 1000 ? window.innerWidth / 2 : window.innerWidth / 3;
-        const originX = window.innerWidth / 2
-        const originY = window.innerWidth / 3
+        const originX = window.innerWidth / 2;
+        const originY = window.innerWidth / 3;
 
         return {
             x: {
@@ -57,24 +55,22 @@ export class Orb extends OrbLike {
         const xNoise = simplex.noise2D(this.xOff, this.xOff);
         const yNoise = simplex.noise2D(this.yOff, this.yOff);
         const scaleNoise = simplex.noise2D(this.xOff, this.yOff);
-      
+
         this.x = map(xNoise, -1, 1, this.bounds["x"].min, this.bounds["x"].max);
         this.y = map(yNoise, -1, 1, this.bounds["y"].min, this.bounds["y"].max);
         this.scale = map(scaleNoise, -1, 1, 0.5, 1);
-      
+
         this.xOff += this.inc;
         this.yOff += this.inc;
-      }
+    }
 
-      render() {
+    render() {
         this.graphics.x = this.x;
         this.graphics.y = this.y;
         this.graphics.scale.set(this.scale);
-      
         this.graphics.clear();
-      
         this.graphics.beginFill(this.fill);
         this.graphics.drawCircle(0, 0, this.radius);
         this.graphics.endFill();
-      }
+    }
 }
